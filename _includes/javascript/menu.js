240b6cdf67996676f8454b0009438391d0e94051
Ryan Arnaudin <arnaudin@users.noreply.github.com>
2025-01-05 16:02:31 -0600

    Adjust mobile menu

--- _includes/javascript/menu.js
+++ _includes/javascript/menu.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('.mobile-nav').on('click', function(){
     $('.navigation').toggleClass('show');
     $('.mobile-nav').html(function(i, label){
-      return label === 'Navigate to...' ? '&times; Close Menu' : 'Navigate to...'
+      return label === 'Menu' ? '&times; Close' : 'Menu'
     });
     $("body").toggleClass("prevent-scroll-mobile");
     $("header.main-header").toggleClass("header-prevent-hide");
