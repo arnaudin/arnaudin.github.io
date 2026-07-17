1eca65716afb834398b3d0600cc259dad0d14dc8
Ryan Arnaudin <arnaudin@users.noreply.github.com>
2026-07-16 20:52:56 -0500

    open external links in new tab

--- _includes/javascript/site.js
+++ _includes/javascript/site.js
@@ -2,6 +2,14 @@
 {% include /javascript/menu.js %}
 {% include /javascript/theme.js %}
 
+// Open external links in a new tab (rel guards against tabnabbing)
+document.querySelectorAll('a[href^="http"]').forEach(function (link) {
+  if (link.hostname !== window.location.hostname) {
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+  }
+});
+
 Headspace(document.querySelector('[data-header]'), {
   startOffset: 90,
   tolerance: 5,
